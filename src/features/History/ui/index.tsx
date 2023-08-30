ca79642509eea16f59d6942936558a50052c3844
PaulLyu <polina.lyubeznova@geogracom.com>
2023-08-30 15:35:58 +0300

    style(features/History): Исправила стили с styles-component на css

--- src/features/History/ui/index.tsx
+++ src/features/History/ui/index.tsx
@@ -1,68 +1,20 @@
-import {
-  createStyles,
-  rem,
-  UnstyledButton,
-  Text,
-  Group,
-  getStylesRef,
-} from "@mantine/core";
+import { createStyles, Text, Group, getStylesRef, Flex } from "@mantine/core";
 import { IconCircle, IconPencil, IconLine } from "@tabler/icons-react";
 import { useState } from "react";
 
 const useStyles = createStyles((theme) => ({
-  actions: {
-    paddingLeft: `calc(${theme.spacing.md} - ${theme.spacing.xs})`,
-    paddingRight: `calc(${theme.spacing.md} - ${theme.spacing.xs})`,
-    paddingBottom: theme.spacing.md,
-  },
-
   action: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    fontSize: theme.fontSizes.xs,
-    padding: `${rem(8)} ${theme.spacing.xs}`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[0]
-        : theme.colors.gray[7],
-
     "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-      color: theme.colorScheme === "dark" ? theme.white : theme.black,
+      backgroundColor: theme.colors.gray[0],
+      color: theme.black,
     },
-  },
-
-  actionInner: {
-    display: "flex",
-    alignItems: "center",
-  },
-
-  actionIcon: {
-    marginRight: theme.spacing.sm,
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[2]
-        : theme.colors.gray[6],
-  },
-
-  historyHeader: {
-    paddingLeft: `calc(${theme.spacing.md} + ${rem(2)})`,
-    paddingRight: theme.spacing.md,
-    marginBottom: rem(5),
   },
 
   actionActive: {
     "&, &:hover": {
-      color: theme.fn.variant({ variant: "light", color: "#F8F8F8" }).color,
+      color: "#a0a0a0",
       [`& .${getStylesRef("icon")}`]: {
-        color: theme.fn.variant({ variant: "light", color: "#F8F8F8" }).color,
+        color: "#a0a0a0",
       },
     },
   },
@@ -79,28 +31,42 @@ export function History() {
   const [active, setActive] = useState(1);
 
   const mainActions = actions.map((action) => (
-    <UnstyledButton
+    <Group
       key={action.id}
+      position="apart"
+      fz="xs"
+      fw="500"
+      p="xs"
+      w="100%"
       className={cx(classes.action, {
         [classes.actionActive]: action.id === active,
       })}
       onClick={() => setActive(action.id)}
     >
-      <div className={classes.actionInner}>
-        <action.icon size={20} className={classes.actionIcon} stroke={1.5} />
+      <Group>
+        <action.icon size={20} stroke={1.5} />
         <span>{action.label}</span>
-      </div>
-    </UnstyledButton>
+      </Group>
+    </Group>
   ));
 
   return (
     <>
-      <Group className={classes.historyHeader} position="apart">
+      <Group px="md" mb="xs" position="apart">
         <Text size="xs" weight={500} color="dimmed">
           История
         </Text>
       </Group>
-      <div className={classes.actions}>{mainActions}</div>
+      <Flex
+        px="xs"
+        mb="xs"
+        justify="center"
+        align="flex-start"
+        direction="column"
+        wrap="wrap"
+      >
+        {mainActions}
+      </Flex>
     </>
   );
 }
