16137d0e1e95b81e6365869e41319ce6ec2685ae
PaulLyu <polina.lyubeznova@geogracom.com>
2023-09-01 01:04:36 +0300

    feat(entities/Zoom): создала панель для инструмента zoom

--- src/widgets/BottomBar/ui/index.tsx
+++ src/widgets/BottomBar/ui/index.tsx
@@ -1,25 +1,29 @@
 import "./index.css";
 import { Card } from "@mantine/core";
 import { IconBrush, IconEraser } from "@tabler/icons-react";
+import { Zoom } from "entities/Zoom";
 import IconButton from "shared/IconButton/ui";
 
 export function BottomBar() {
   return (
-    <Card
-      display="flex"
-      shadow="sm"
-      padding="sm"
-      radius="md"
-      className="bottombar_card"
-    >
-      <IconButton
-        icon={<IconBrush size="1rem" color="black" />}
-        variant={"subtle"}
-      />
-      <IconButton
-        icon={<IconEraser size="1rem" color="black" />}
-        variant={"subtle"}
-      />
-    </Card>
+    <>
+      <Card
+        display="flex"
+        shadow="sm"
+        padding="sm"
+        radius="md"
+        className="bottombar_card"
+      >
+        <IconButton
+          icon={<IconBrush size="1rem" color="black" />}
+          variant={"subtle"}
+        />
+        <IconButton
+          icon={<IconEraser size="1rem" color="black" />}
+          variant={"subtle"}
+        />
+      </Card>
+      <Zoom />
+    </>
   );
 }
