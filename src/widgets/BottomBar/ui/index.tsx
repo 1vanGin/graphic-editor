a0f2c9591814de41624ba0a0275945c4e55d85ab
Polina Lyubeznova <polina.lyubeznova@geogracom.com>
2023-09-01 15:58:31 +0300

    fix(widgets/BottomBar): Заменила компонент Card на компонент Flex

--- src/widgets/BottomBar/ui/index.tsx
+++ src/widgets/BottomBar/ui/index.tsx
@@ -1,29 +1,13 @@
 import "./index.css";
-import { Card } from "@mantine/core";
-import { IconBrush, IconEraser } from "@tabler/icons-react";
+import { Flex } from "@mantine/core";
 import { Zoom } from "entities/Zoom";
-import IconButton from "shared/IconButton/ui";
 
 export function BottomBar() {
   return (
     <>
-      <Card
-        display="flex"
-        shadow="sm"
-        padding="sm"
-        radius="md"
-        className="bottombar_card"
-      >
-        <IconButton
-          icon={<IconBrush size="1rem" color="black" />}
-          variant={"subtle"}
-        />
-        <IconButton
-          icon={<IconEraser size="1rem" color="black" />}
-          variant={"subtle"}
-        />
-      </Card>
-      <Zoom />
+      <Flex gap="md" className="bottombar_card">
+        <Zoom />
+      </Flex>
     </>
   );
 }
