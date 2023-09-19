316b0e55e3c03b82ea422cb82908605a57677272
DENIS LOGINOV <denis.lkg@gmail.com>
2023-09-19 22:08:56 +0600

    fix: run test

--- spec/integration/App.spec.tsx
+++ spec/integration/App.spec.tsx
@@ -1,10 +1,13 @@
 import { render } from "@testing-library/react";
 import { JestStoreProvider } from "../utils/JestStoreProvider.tsx";
 import App from "app";
+import { act } from "react-dom/test-utils";
 
-it("Проверка корневого компонента", () => {
-  render(<App />, {
-    wrapper: JestStoreProvider,
+it("Проверка корневого компонента", async () => {
+  await act(async () => {
+    render(<App />, {
+      wrapper: JestStoreProvider,
+    });
   });
   expect(true).toBeTruthy();
 });
