def0b57fa161299721152d1e35fc00b40d333ca5
PaulLyu <polina.lyubeznova@geogracom.com>
2023-09-22 23:59:21 +0300

    fix(features/Layers/model/layersThunk): Первый слой активен по дефолту

--- src/features/Layers/model/layersThunk.ts
+++ src/features/Layers/model/layersThunk.ts
@@ -2,17 +2,24 @@
 import { createAction } from "@reduxjs/toolkit";
 import { AppThunk } from "app/store/store";
 import { ILayer } from "entities/LayersItem";
+import { setActiveLayer } from "./slice";
 
-export const setProjectLayersThunk = createAction<ILayer[]>("layers/setProjectLayersThunk");
+export const setProjectLayersThunk = createAction<ILayer[]>(
+  "layers/setProjectLayersThunk"
+);
 
 export const setProjectLayers =
   (projectId: string): AppThunk =>
   (dispatch, getState) => {
     const state = getState();
-    const project = state.projects.projects.filter((project) => project.id === projectId);
+    const project = state.projects.projects.filter(
+      (project) => project.id === projectId
+    );
     const layers = project[0]?.layers;
     if (!layers) return;
-    dispatch(
-      setProjectLayersThunk(Object.values(layers).sort((a, b) => a.sortOrder - b.sortOrder))
+    const sortedLayers = Object.values(layers).sort(
+      (a, b) => a.sortOrder - b.sortOrder
     );
+    dispatch(setProjectLayersThunk(sortedLayers));
+    dispatch(setActiveLayer(sortedLayers[0]));
   };
