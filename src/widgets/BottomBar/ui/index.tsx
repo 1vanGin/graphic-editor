81fdd03a85e626ca0c97400561de80c096281693
Ruslan Skoloty <RSkoloty@renins.com>
2023-09-23 11:48:09 +0300

    feat[BottomBar]: Добавил горячие клавиши на отмену/возврат действия

--- src/widgets/BottomBar/ui/index.tsx
+++ src/widgets/BottomBar/ui/index.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from "app/store/hooks";
 import { UndoRedo } from "entities/UndoRedo";
 import { Zoom } from "entities/Zoom";
 import { redo, undo } from "features/History";
+import { useHotkeys } from 'react-hotkeys-hook';
 
 export function BottomBar() {
   const dispatch = useAppDispatch();
@@ -14,6 +15,10 @@ export function BottomBar() {
   const handleRedoClick = () => {
     dispatch(redo());
   };
+
+  useHotkeys('ctrl+z', handleUndoClick);
+  useHotkeys('ctrl+y', handleRedoClick);
+
   return (
     <>
       <Flex gap="md" className="bottombar_card">
